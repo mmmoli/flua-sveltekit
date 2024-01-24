b6cabf6a693be4951692be5a346203f000ce6c88
Michele Memoli <michele.memoli@gmail.com>
2024-01-24 18:40:54 +0000

    chore(app): removed wip

--- tmp/account/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { AccountPage } from '~pages/account-page';
-	import type { PageData } from '../account/$types';
-	export let data: PageData;
-</script>
-
-<AccountPage {data} />
--- tmp/join/[room_slug]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { CallPage } from '~pages/call-page';
-	import type { PageData } from '../../join/[room_slug]/$types';
-	export let data: PageData;
-</script>
-
-<CallPage {data} />
